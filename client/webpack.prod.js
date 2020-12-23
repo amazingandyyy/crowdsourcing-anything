42c72a19d566be1a3ec57f5c5a4a4c243c3ee8b0
Andy Chen <amazingandyyy@gmail.com>
2020-12-23 11:28:43 -0800

    feat: add useNewUrlParser

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                'API_URI': JSON.stringify('https://mern-amazingandyyy.herokuapp.com')
+                'API_URI': JSON.stringify('https://crowdsourcing-anything.herokuapp.com')
             },
         }),
     ]
